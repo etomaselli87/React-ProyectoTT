44c7acdc1f7180270096c8a2dd580c488f549ef4
Emmanuel Tomaselli <e.tomaselli@yahoo.com.ar>
2026-05-11 11:55:52 -0300

    Preparar Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { Header } from './components/Header/Header'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
 
 
   return (
     <>
-      
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+        </Routes>
+      </main>
+      <Footer />
     </>
   )
 }
