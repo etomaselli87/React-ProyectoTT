e26c752c05f887e21d1a47f7c1d53eeca2b5ae02
Emmanuel Tomaselli <e.tomaselli@yahoo.com.ar>
2026-05-11 11:57:01 -0300

    footer .jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>2026 © Matrika - Sitio desarrollado por Ema</p>
+      <nav>
+        <ul className="nav-list">
+          <li>Whatsapp</li>
+          <li>Instagram</li>
+        </ul>
+      </nav>
+    </footer>
+  );
+};
