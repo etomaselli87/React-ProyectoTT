881fc60792be9ecaf1a18b9d73a5033f2649b2c2
Emmanuel Tomaselli <e.tomaselli@yahoo.com.ar>
2026-05-13 08:28:16 -0300

    Agregado links redes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,15 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer>
-      <p>2026 © Matrika - Sitio desarrollado por Ema</p>
+      <p>2026 © Matrika - Sitio desarrollado por Ema T.</p>
       <nav>
         <ul className="nav-list">
-          <li>Whatsapp</li>
-          <li>Instagram</li>
+          <li>
+            <a href="https://wa.me/5491157197206" target="_blank" rel="noopener noreferrer">Whatsapp</a>
+          </li>
+          <li>
+            <a href="https://instagram.com/matrika.esencia" target="_blank" rel="noopener noreferrer">Instagram</a>
+          </li>
         </ul>
       </nav>
     </footer>
