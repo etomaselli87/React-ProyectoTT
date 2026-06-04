0618e37c57c689a691ab172ba8fa2de79aa52ac1
Emmanuel Tomaselli <e.tomaselli@yahoo.com.ar>
2026-06-04 11:10:11 -0300

    Agregado clave a archivo adjunta tipo de errores

---
+++ src/utils/validateProduct.js
@@ -0,0 +1,28 @@
+//Se arma objeto y en caso de que entra en algun condicional, LE AGREGA UNA CLAVE X, 
+// y retorna un objeto final errors con descripcion de Estado de Errores
+
+export const validateProduct = (product) => {
+  const errors = {};
+
+  if (!product.name.trim()) {
+    errors.name = "El nombre es obligatorio";
+  }
+
+  if (!product.price || product.price <= 0) {
+    errors.price = "El precio debe ser mayor que 0";
+  }
+
+  if (!product.description.trim()) {
+    errors.description = "La descripción es obligatoria";
+  }
+
+  if (!product.category.trim()) {
+    errors.category = "La categoria es obligatoria";
+  }
+
+  if (!product.file) {
+    errors.file = "Debes seleccionar una imagen";
+  }
+
+  return errors;
+};
