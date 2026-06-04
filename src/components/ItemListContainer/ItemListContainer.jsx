9cbbea63aef6c4cd524db4320f59b49013b160f5
Emmanuel Tomaselli <e.tomaselli@yahoo.com.ar>
2026-06-04 11:07:57 -0300

    Agregado filtro category

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import { ItemList } from "../ItemList/ItemList";
+import { getByCategory, getProductById, getProducts } from "../../services/productService";
+import { useParams } from "react-router-dom";
 
 export const ItemListContainer = () => {
+  const { category } = useParams()
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -9,12 +12,19 @@ export const ItemListContainer = () => {
   useEffect(() => {
     setLoading(true);
 
-    fetch("/data/products.json")
-      .then((res) => res.json())
+    // SACAMOS EL FETCH QUE PETICIONA AL JSON. PARA HACER LA CONEXION CON FILESTORE (getProducts)
+    // fetch("/data/products.json")
+    //   .then((res) => res.json())
+
+    //getProducts() o por category====>
+      //Usamos la funcion con 2 usos: Trae todo o filtra si category existe
+      getByCategory(category)
       .then((data) => setProducts(data))
       .catch((err) => console.log("Hubo un error:", err))
       .finally(() => setLoading(false));
-  }, []);
+  }, [category]);
+  //👉No olvidar agregar [category] como variable de dependencias para que se renderice nuevamente 
+  // el container si cambia category
 
   //console.log(products);
 
