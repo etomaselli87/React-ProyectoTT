89d361cca43ebcaa7a8a9c25d4ace3481e10c56e
Emmanuel Tomaselli <e.tomaselli@yahoo.com.ar>
2026-06-04 11:04:52 -0300

    admin manual provisorio en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { Footer } from './components/Footer/Footer'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer'
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer'
 import { CartView } from './components/Cart/CartView'
+import { ProductFormContainer } from './components/adminComponents/ProductFormContainer'
+import { ProductSuccess } from './components/adminComponents/ProductSuccess'
 
 function App() {
 
@@ -16,8 +18,14 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<ItemListContainer />} />
+          {/* Ruta para filtrar por category */}
+          <Route path="category/:category" element={<ItemListContainer />} />
           <Route path="/product/:id" element={<ItemDetailContainer />} />
           <Route path="/carrito" element={<CartView />} />
+
+          {/* Provisorio escribir MANUAL /admin para subir formulario de productos */}
+          <Route path="/admin" element={<ProductFormContainer />} />
+          <Route path="/success/:id" element={<ProductSuccess />} />
         </Routes>
       </main>
       <Footer />
