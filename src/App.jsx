0d3e0a8a8fa6c8e15a07b2ba738fd2ccec3ba19f
Emmanuel Tomaselli <e.tomaselli@yahoo.com.ar>
2026-06-10 11:43:59 -0300

    Rutas públicas y privadas (admin -protegidas) en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,53 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import { Header } from './components/Header/Header'
-import { Footer } from './components/Footer/Footer'
+// import { Header } from './components/Header/Header'
+// import { Footer } from './components/Footer/Footer'
 
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer'
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer'
 import { CartView } from './components/Cart/CartView'
 import { ProductFormContainer } from './components/adminComponents/ProductFormContainer'
 import { ProductSuccess } from './components/adminComponents/ProductSuccess'
+import { PublicLayout } from './layouts/PublicLayout'
+import { AdminLayout } from './layouts/AdminLayout'
+import { Dashboard } from "./components/adminComponents/Dashboard/Dashboard";
+
 
 function App() {
 
 
   return (
     <>
-      <Header />
-      <main>
         <Routes>
-          <Route path="/" element={<ItemListContainer />} />
-          {/* Ruta para filtrar por category */}
-          <Route path="category/:category" element={<ItemListContainer />} />
-          <Route path="/product/:id" element={<ItemDetailContainer />} />
-          <Route path="/carrito" element={<CartView />} />
+          /* ----------------------------- RUTAS PÚBLICAS ----------------------------- */
+          <Route element={<PublicLayout />}>
+            <Route path="/" element={<ItemListContainer />} />
+            {/* Ruta para filtrar por category */}
+            <Route path="category/:category" element={<ItemListContainer />} />
+            <Route path="/product/:id" element={<ItemDetailContainer />} />
+            <Route path="/carrito" element={<CartView />} />
+          </Route>
 
-          {/* Provisorio escribir MANUAL /admin para subir formulario de productos */}
-          <Route path="/admin" element={<ProductFormContainer />} />
-          <Route path="/success/:id" element={<ProductSuccess />} />
+          <Route path="admin/login" element={<Login />} />
+          /* ---------------------------------- ADMIN --------------------------------- */
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminLayout />
+              </ProtectedRoute>
+          }
+        >
+            {/* Si el admin esta logueado, redirige a la ruta /admin/dashboard */}
+            <Route index element={<Navigate to={"dashboard"} />} />
+            <Route path="dashboard" element={<Dashboard />} />
+
+            <Route path="/new" element={<ProductFormContainer />} />
+            <Route path="/products/success/:id" element={<ProductSuccess />} />
+          </Route>
         </Routes>
-      </main>
-      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default App
