deb3197b19220f2ad9b681883847b5f791b1ca10
Emmanuel Tomaselli <e.tomaselli@yahoo.com.ar>
2026-06-15 18:07:56 -0300

    Modificacion eliminar "/" dps de /admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,51 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-// import { Header } from './components/Header/Header'
-// import { Footer } from './components/Footer/Footer'
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import { ItemListContainer } from './components/ItemListContainer/ItemListContainer'
-import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer'
-import { CartView } from './components/Cart/CartView'
-import { ProductFormContainer } from './components/adminComponents/ProductFormContainer'
-import { ProductSuccess } from './components/adminComponents/ProductSuccess'
-import { PublicLayout } from './layouts/PublicLayout'
-import { AdminLayout } from './layouts/AdminLayout'
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
+import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
+import { CartView } from "./components/Cart/CartView";
+import { ProductFormContainer } from "./components/adminComponents/ProductFormContainer";
+import { ProductSuccess } from "./components/adminComponents/ProductSuccess";
+import { PublicLayout } from "./layouts/PublicLayout";
+import { AdminLayout } from "./layouts/AdminLayout";
+import { ProtectedRoute } from "./components/ProtectedRoute/ProtectedRoute";
 import { Dashboard } from "./components/adminComponents/Dashboard/Dashboard";
 
-
 function App() {
-
-
   return (
     <>
-        <Routes>
-          /* ----------------------------- RUTAS PÚBLICAS ----------------------------- */
-          <Route element={<PublicLayout />}>
-            <Route path="/" element={<ItemListContainer />} />
-            {/* Ruta para filtrar por category */}
-            <Route path="category/:category" element={<ItemListContainer />} />
-            <Route path="/product/:id" element={<ItemDetailContainer />} />
-            <Route path="/carrito" element={<CartView />} />
-          </Route>
+      <Routes>
+        {/* ----------------------RUTAS PUBLICAS -------------------------*/}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<ItemListContainer />} />
+          {/* Ruta opcional para filtrar categorias */}
+          <Route path="/category/:category" element={<ItemListContainer />} />
+          <Route path="/product/:id" element={<ItemDetailContainer />} />
+          <Route path="/carrito" element={<CartView />} />
+        </Route>
 
-          <Route path="admin/login" element={<Login />} />
-          /* ---------------------------------- ADMIN --------------------------------- */
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute>
-                <AdminLayout />
-              </ProtectedRoute>
+        {/* IMPORTAR EL COMPONENTE LOGIN */}
+        {/* <Route path="/admin/login" element={<Login />} /> */}
+        <Route path="/admin/login" element={<h2>LOGIN</h2>} />
+        {/* -----------------------------ADMIN--------------------------- */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminLayout />
+            </ProtectedRoute>
           }
         >
-            {/* Si el admin esta logueado, redirige a la ruta /admin/dashboard */}
-            <Route index element={<Navigate to={"dashboard"} />} />
-            <Route path="dashboard" element={<Dashboard />} />
+          {/* Si el admin esta logueado, redirige a la ruta /admin/dashboard */}
+          <Route index element={<Navigate to={"dashboard"} />} />
+          <Route path="dashboard" element={<Dashboard />} />
 
-            <Route path="/new" element={<ProductFormContainer />} />
-            <Route path="/products/success/:id" element={<ProductSuccess />} />
-          </Route>
-        </Routes>
+          <Route path="products/new" element={<ProductFormContainer />} />
+          <Route path="products/success/:id" element={<ProductSuccess />} />
+        </Route>
+      </Routes>
     </>
   );
-};
+}
 
-export default App
+export default App;
